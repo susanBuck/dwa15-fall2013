ca895e067addbfee24c5dab961f9699f36a7c2ff
Susan Buck <susan@susanbuck.net>
2013-10-28 08:14:56 -0400

    Watchr script

---
+++ watchr.rb
@@ -0,0 +1,8 @@
+# Command to run this: watchr /Users/Susan/Sites/dwa15-fall2013/watchr.rb
+
+# system %(echo "Running watchr.rb")
+ 
+# Run the shell script
+%x('/Users/Susan/Sites/dwa15-fall2013/watchr')
+
+
